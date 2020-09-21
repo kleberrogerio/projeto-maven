7af9f139dd1898f29bb77f09f64af3348effe7c0
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2020-09-21 01:07:50 +0000

    Continuação JPA

---
+++ src/main/java/br/gov/sp/fatec/projetomaven/entity/Aluno.java
@@ -0,0 +1,33 @@
+package br.gov.sp.fatec.projetomaven.entity;
+
+public class Aluno {
+    private Long id;
+    private String nomeUsuario;
+    private String senha;
+    private Long ra;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
