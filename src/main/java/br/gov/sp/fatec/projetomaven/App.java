ed5eabfd5050f03bc2f4a00affedff35cffb1ac8
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2020-09-28 01:51:42 +0000

    Terminando a parte 2

--- src/main/java/br/gov/sp/fatec/projetomaven/App.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/App.java
@@ -19,6 +19,36 @@ public class App
     {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("avaliacao");
         EntityManager manager = factory.createEntityManager();
+        Aluno aluno = new Aluno();
+        aluno.setNomeUsuario("alunofatec");
+        aluno.setSenha("senha");
+        aluno.setRa(1234567891011L);
+
+        Professor professor = new Professor();
+        professor.setNomeUsuario("professorlabiv");
+        professor.setSenha("senhaForte");
+
+        Trabalho trabalho = new Trabalho();
+        trabalho.setTitulo("Trabalho 2 Lab IV");
+        trabalho.setDataHoraEntrega(new Date());
+        trabalho.setLocalArquivo("trabalhos");
+        trabalho.setAvaliador(professor);
+        trabalho.setAlunos(new HashSet<Aluno>());
+        trabalho.getAlunos().add(aluno);
+
+        try{
+            manager.getTransaction().begin();
+            manager.persist(aluno);
+            manager.persist(professor);
+            manager.persist(trabalho);
+            manager.getTransaction().commit();
+            }
+        catch(PersistenceException e){
+            e.printStackTrace();
+            manager.getTransaction().rollback();
+        }
+        manager.clear();
+
         /*
         Aluno aluno = new Aluno();
         aluno.setNomeUsuario("aluno");
@@ -39,12 +69,21 @@ public class App
             manager.getTransaction().rollback();
         }
         */
-        Aluno aluno = manager.find(Aluno.class,1L);
+        
+        aluno = manager.find(Aluno.class, aluno.getId);
         System.out.println(aluno.getId());
         System.out.println(aluno.getNomeUsuario());  
-        for(Trabalho trabalho: aluno.getTrabalhos()) {
-            System.out.println(trabalho.getTitulo());
+        for(Trabalho trab: aluno.getTrabalhos()) {
+            System.out.println(trab.getTitulo());
         }
+        manager.clear();
+        trabalho = manager.find(Trabalho.class,trabalho.getId);
+        System.out.println(trabalho.getTitulo());
+        for(Aluno al: trabalho.getAlunos()){
+            System.out.println(al.getNomeUsuario());
+        }
+
+
         manager.close();    	
     }
 }
