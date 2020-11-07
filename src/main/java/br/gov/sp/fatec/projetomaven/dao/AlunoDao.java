98dc283ffd2363daac4bb18e3f5a9824a69da04c
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2020-11-07 01:09:50 +0000

    Padroes

--- src/main/java/br/gov/sp/fatec/projetomaven/dao/AlunoDao.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/dao/AlunoDao.java
@@ -1,10 +1,12 @@
 package br.gov.sp.fatec.projetomaven.dao;
 
+import br.gov.sp.fatec.projetomaven.entity.Aluno;
+
 public interface AlunoDao{
     public Aluno cadastrarAluno(String nomeUsuario, String senha, Long ra);
 
     public Aluno buscarAlunoPorRa(Long ra);
 
     public void removerAluno(Long ra); 
-    
+
 }
--- src/main/java/br/gov/sp/fatec/projetomaven/dao/AlunoDaoJpa.java
+++ src/main/java/br/gov/sp/fatec/projetomaven/dao/AlunoDaoJpa.java
@@ -1,5 +1,7 @@
 package br.gov.sp.fatec.projetomaven.dao;
 
 public class AlunoDaoJpa implements AlunoDao{
-    
+
+    @Override
+    public Aluno cadastrarAluno(String nom)
 }
